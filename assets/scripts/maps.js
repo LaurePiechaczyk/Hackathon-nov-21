03e98dfbd8bdbc2e5219c65a43cd9d3078c29687
mcglasp <nmglasper@gmail.com>
2021-11-15 14:50:21 +0000

    Alert function added for when a user clicks on the map link for an online event

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,9 +1,15 @@
+window.onload = function() {
+  localStorage.removeItem("city")
+};
+
 function citySet(selectedCity) {
   return localStorage.setItem("city", selectedCity)
 }
 
 function initMap() {
   let city = localStorage.getItem("city")
+
+  console.log(city)
   let locations;
   let map = new google.maps.Map(document.getElementById('map'));
 
@@ -55,6 +61,50 @@ function initMap() {
           lng: -6.253264302810794
         }
       ]
+      break;
+      case "Online":
+        alert("This is an online event, but why not browse our in-person events too?")
+        locations = [{
+          label: "Coffee & Science",
+          lat: 51.51130051843234,
+          lng: -0.1264144301168318
+        },
+        {
+          label: "Lecture",
+          lat: 51.52558226631843,
+          lng: -0.1221202043368674
+        },
+        {
+          // laboratory
+          label: "Laboratory open day",
+          lat: 51.5249418889357,
+          lng: -0.13250695188833084
+        },
+        {
+          // laboratory
+          label: "Laboratory open day",
+          lat: 55.908466874609694,
+          lng: -4.312559023261887
+        },
+        {
+          // lecture theatre
+          label: "Lecture",
+          lat: 55.862924151045334,
+          lng: -4.248479000542656
+        },
+        {
+          label: "Laboratory Open Day",
+          lat: 53.39413320626776,
+          lng: -6.359222602385262
+        },
+        {
+          // cafe
+          label: "Coffee & Science",
+          lat: 53.35098396883487,
+          lng: -6.253264302810794
+        }
+      ]
+
       break;
     default:
       locations = [{
@@ -109,8 +159,9 @@ function initMap() {
       label: location.label
     });
   });
-
+ 
   var markerCluster = new MarkerClusterer(map, markers, {
     imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
+
 }
